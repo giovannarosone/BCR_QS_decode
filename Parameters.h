1f528ae73c78046693f3357b38a681b8c4ee261e
bfqzip <guerrini.veronica55@gmail.com>
2024-01-24 15:01:16 +0100

    Update Parameters.h

--- Parameters.h
+++ Parameters.h
@@ -44,7 +44,7 @@
 #define SIZEBUFFER 1024     //Size of the buffer for I/O partial ebwt/LCP/DA/SA
 
 #define TERMINATE_CHAR '#'     //it is the symbol used as "end of strings", it must be lexicographically smaller than all the letters of the alphabet
-#define TERMINATE_CHAR_LEN '$'      //it is stored in cyc files, it is ignored by the algorithm, so it must not belong to the alphabet
+#define TERMINATE_CHAR_LEN uchar(255)      //it is stored in cyc files, it is ignored by the algorithm, so it must not belong to the alphabet
 
 #define SIZE_ALPHA 256  
 
